dda66b6563903bce32b023c04819f41945946b42
LeeSawyer <leesawyer101@gmail.com>
2019-03-22 22:14:37 -0700

    Updated some files

--- fltk/projectLeeSawyer.cpp
+++ fltk/projectLeeSawyer.cpp
@@ -14,18 +14,27 @@ void horizontalWalls(int i, int j){
   maze[i][j]->box(FL_UP_BOX);
   maze[i][j]->color(FL_RED);
 }
-// void noVerticalWall(int i, int j){
-//   maze[i][j] = new Fl_Box(((((j-1)/2)*100) + 110),((((i-1)/2)*100) + 20),10,90,"");
-//   maze[i][j]->box(FL_FLAT_BOX);
-//   maze[i][j]->color(FL_WHITE);
-// }
-// void horizontalWalls(int i, int j){
-//   maze[i][j] = new Fl_Box(((((j-1)/2)*100) + 20),((((i-1)/2)*100) + 110),90,10,"");
-//   maze[i][j]->box(FL_FLAT_BOX);
-//   maze[i][j]->color(FL_WHITE);
-// }
+void noVerticalWalls(int i, int j){
+  maze[i][j] = new Fl_Box(((((j-1)/2)*100) + 110),((((i-1)/2)*100) + 20),10,90,"");
+  maze[i][j]->box(FL_FLAT_BOX);
+  maze[i][j]->color(FL_WHITE);
+  //std::cout << "what " ;//((j%2) == 0) << " ";
+}
+void noHorizontalWalls(int i, int j){
+  maze[i][j] = new Fl_Box(((((j-1)/2)*100) + 20),((((i-1)/2)*100) + 110),90,10,"");
+  maze[i][j]->box(FL_FLAT_BOX);
+  maze[i][j]->color(FL_WHITE);
+}
 
 void makeMaze() {
+  for (int i = 0; i < 13; i++){
+    for (int j = 0; j < 13; j++){
+        if(((i%2)==1) && ((j%2) == 0))//placing No vertical walls
+          noVerticalWalls(i,j);
+        if(((i%2)==0) && ((j%2) == 1))//placing No vertical walls
+          noHorizontalWalls(i,j);
+    }
+  }
         ///////////vertical walls
         verticalWall(1,2);
         verticalWall(3,2);
@@ -82,20 +91,46 @@ void makeMaze() {
           maze[12][j]->color(FL_RED);
           //std::cout << i;
         }
-        // if(((i%2)==1) && ((j%2)==1)){//placing No vertical walls
-        //   if(maze[i][j]->color() != FL_RED)
-        //     noVerticalWall(i,j);
-        // }
+        if(((i%2)==1) && ((j%2)==1)){//placing each block
+          maze[i][j] = new Fl_Box(((((j-1)/2)*100) + 20),((((i-1)/2)*100) + 20),90,90,"");
+          maze[i][j]->box(FL_FLAT_BOX);
+          maze[i][j]->color(FL_WHITE);
+        }
     }
 
   }
+  maze[11][11]->label("END");
 
+  Fl_Box *stateBoxLabel = new Fl_Box(800,50,30,50,"State");
+  stateBoxLabel->box(FL_NO_BOX);
+  stateBoxLabel->labelfont(FL_BOLD);
+  stateBoxLabel->labelsize(12);
+  Fl_Box *stateBox = new Fl_Box(700,100,250,50,"");
+  stateBox->box(FL_DOWN_BOX);
+  stateBox->labelfont(FL_BOLD);
+  stateBox->labelsize(12);
 
+  Fl_Box *processBoxLabel = new Fl_Box(800,250,30,50,"Process");
+  processBoxLabel->box(FL_NO_BOX);
+  processBoxLabel->labelfont(FL_BOLD);
+  processBoxLabel->labelsize(12);
+  Fl_Box *processBox = new Fl_Box(700,300,250,50,"");
+  processBox->box(FL_DOWN_BOX);
+  processBox->labelfont(FL_BOLD);
+  processBox->labelsize(12);
 
+  Fl_Box *eventBoxLabel = new Fl_Box(800,450,30,50,"Event");
+  eventBoxLabel->box(FL_NO_BOX);
+  eventBoxLabel->labelfont(FL_BOLD);
+  eventBoxLabel->labelsize(12);
+  Fl_Box *eventBox = new Fl_Box(700,500,250,50,"");
+  eventBox->box(FL_DOWN_BOX);
+  eventBox->labelfont(FL_BOLD);
+  eventBox->labelsize(12);
 
 }
 
-int main(int argc, char ** argv) {//helloworld.cpp:18:16: error: ‘argc’ was not declared in this sco
+int main(int argc, char ** argv) {
   
   
   Fl_Window *window = new Fl_Window(1000,630);
